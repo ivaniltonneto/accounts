736021fcf2c2457691c3d94483ea81e9c6db499b
Ivanilton Neto <netijua@gmail.com>
2023-02-22 17:16:32 -0300

    feat: all operations created and functional

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# Developing a banking system with only core modules
+<h3 align="center">
+  Developing a banking system with only core modules
+</h3>
+
+<p align="center">
+  <a href="#-how-to-use">How to Use</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+</p>
+
+<p align="center">
+  <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/ivaniltonneto/accounts">
+
+  <img alt="GitHub top language" src="https://img.shields.io/github/languages/top/ivaniltonneto/accounts">
+
+  <img alt="GitHub repo size" src="https://img.shields.io/github/repo-size/ivaniltonneto/accounts">
+
+  <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/ivaniltonneto/accounts">
+
+</p>
+
+<br/>
+
+## ℹ️ How To Use
+
+To clone and run this application, you'll need [git](https://git-scm.com), [npm](https://www.npmjs.com/), [yarn](https://yarnpkg.com)
+<br/>From your command line:
+
+```bash
+# Clone the repository
+$ git clone https://github.com/ivaniltonneto/accounts
+```
+
+### ⚙️ Server API
+
+```bash
+# Go into the repository
+$ cd accounts
+
+# Install dependencies
+$ npm install
+
+# Run the application
+$ npm start
+
+```
+---
+
+Made with ♥ by Ivanilton Neto - [Get in touch](https://www.linkedin.com/in/ivanilton-neto-a712a379/)
--- index.js
+++ index.js
@@ -20,17 +20,20 @@ const operation = () => {
     ])
     .then((answer) => {
       const action = answer.action;
-      if (action === 'Criar conta') {createAccount()}
-      else if(action === 'Consultar saldo'){}
-      else if(action === 'Depositar'){
-        deposit()
+      if (action === 'Criar conta') {
+        createAccount();
+      } else if (action === 'Consultar saldo') {
+        getAccountBalance();
+      } else if (action === 'Depositar') {
+        deposit();
+      } else if (action === 'Sacar') {
+        widthdraw();
+      } else if (action === 'Sair') {
+        console.log(
+          chalk.bgBlue.black('Obrigado por usar os nossos serviços!')
+        );
+        process.exit();
       }
-      else if(action === 'Sacar'){}
-      else if(action === 'Sair'){
-        console.log(chalk.bgBlue.black('Obrigado por usar os nossos serviços!'))
-        process.exit()
-      }
-
     })
     .catch((err) => console.log(err));
 };
@@ -82,24 +85,159 @@ const buildAccount = () => {
 };
 
 const deposit = () => {
-  inquirer.prompt([{
-    name: 'accountName',
-    message: 'Qual o nome da sua conta?'
-  }]).then((answer) => {
-    const accountName =  answer.accountName
-     if(!checkAccount(accountName)){
-      return deposit()
-     }
-  })
-}
+  inquirer
+    .prompt([
+      {
+        name: 'accountName',
+        message: 'Qual o nome da conta?',
+      },
+    ])
+    .then((answer) => {
+      const accountName = answer.accountName;
+      if (!checkAccount(accountName)) {
+        return deposit();
+      }
+
+      inquirer
+        .prompt([
+          {
+            name: 'amount',
+            message: 'Valor do deposito',
+          },
+        ])
+        .then((answer) => {
+          const amount = answer.amount;
+
+          addAmount(accountName, amount);
+          operation();
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+};
 
 const checkAccount = (account) => {
-  if(!fs.existsSync(`accounts/${account}.json`)){
+  if (!fs.existsSync(`accounts/${account}.json`)) {
     console.log(chalk.bgRed.black('Está conta não existe, escolha outro nome'));
 
-    return false
+    return false;
   }
-  return true
-}
+  return true;
+};
 
+const addAmount = (accountName, amount) => {
+  const accountData = getAccount(accountName);
+
+  if (!amount) {
+    console.log(
+      chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarde!')
+    );
+    return deposit();
+  }
+
+  accountData.balance += parseFloat(amount);
+
+  fs.writeFileSync(
+    `accounts/${accountName}.json`,
+    JSON.stringify(accountData),
+    function (err) {
+      console.log(err);
+    }
+  );
+
+  console.log(
+    chalk.green(`Foi depositado o valor de R$${amount} na conta ${accountName}`)
+  );
+};
+
+const widthdrawAmoun = (accountName, amount) => {
+  const accountData = getAccount(accountName);
+
+  if (!amount) {
+    console.log(
+      chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarde!')
+    );
+    return widthdraw();
+  }
+
+  accountData.balance -= parseFloat(amount);
+
+  fs.writeFileSync(
+    `accounts/${accountName}.json`,
+    JSON.stringify(accountData),
+    function (err) {
+      console.log(err);
+    }
+  );
+
+  console.log(
+    chalk.green(
+      `Foi realizado um saque no valor de R$${amount} da conta ${accountName}`
+    )
+  );
+};
+
+const getAccount = (accountName) => {
+  const accountJSON = fs.readFileSync(`accounts/${accountName}.json`, {
+    encoding: 'utf-8',
+    flag: 'r',
+  });
+
+  return JSON.parse(accountJSON);
+};
+
+const getAccountBalance = () => {
+  inquirer
+    .prompt([
+      {
+        name: 'accountName',
+        message: 'Qual o nome da conta',
+      },
+    ])
+    .then((answer) => {
+      const accountName = answer.accountName;
+
+      if (!checkAccount(accountName)) {
+        return getAccountBalance();
+      }
+
+      const accountData = getAccount(accountName);
+      console.log(chalk.bgBlue.black(`Seu saldo é: R$${accountData.balance}`));
+      operation();
+    })
+    .catch((err) => console.log(err));
+};
+
+const widthdraw = () => {
+  inquirer
+    .prompt([
+      {
+        name: 'accountName',
+        message: 'Qual o nome da conta',
+      },
+    ])
+    .then((answer) => {
+      const accountName = answer.accountName;
+
+      if (!checkAccount(accountName)) {
+        return widthdraw();
+      }
+
+      inquirer
+        .prompt([
+          {
+            name: 'amount',
+            message: 'Valor do saque',
+          },
+        ])
+        .then((answer) => {
+          const amount = answer.amount;
+
+          widthdrawAmoun(accountName, amount);
+          operation();
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+};
 operation();
